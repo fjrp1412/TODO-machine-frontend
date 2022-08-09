ee22d25cd3bb85f67af4001f641117a09381759b
Francisco Ruiz <fruz.rc@gmail.com>
2022-08-09 12:43:18 -0400

    add more customizable button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ButtonStyles } from './ButtonStyles';
 
-const Button = ({ children, handleClick }) => {
-  return <ButtonStyles type="button" onClick={handleClick}>{children}</ButtonStyles>;
+const Button = ({ children, handleClick, sx }) => {
+  return <ButtonStyles type="button" onClick={handleClick} {...sx} >{children}</ButtonStyles>;
 };
 
 export { Button };
--- src/components/Button/ButtonStyles.jsx
+++ src/components/Button/ButtonStyles.jsx
@@ -1,14 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const ButtonStyles = styled.button`
-    width: 150px;
-    height: 30px;
-    margin: 10px 0px;
-    border: none;
-    border-radius: 25px;
-    background-color: var(--light-blue);
-    color: white;
-    font-size: 1.6rem;
+  width: ${props => props.width};
+  height: ${props => props.height};
+  margin: ${props => props.margin};
+  border: ${props => props.border};
+  border-radius: ${props => props.borderRadius};
+  background-color: ${props => props.backgroundColor};
+  color: ${props => props.color};
+  font-size: ${props => props.fontSize};
 `;
+
+ButtonStyles.defaultProps = {
+  width: '150px',
+  height: '30px',
+  margin: '10px 0px',
+  border: 'none',
+  borderRadius: '25px',
+  backgroundColor: 'var(--light-blue)',
+  color: 'white',
+  fontSize: '1.6rem',
+};
 
 export { ButtonStyles };
