fa54098e6772cf908de394fcc36d6d3709b78096
Francisco Ruiz <fruz.rc@gmail.com>
2022-08-10 09:19:42 -0400

    add Home to routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { Login } from '@pages/Login/Login.jsx';
 export { Signup } from '@pages/Signup/Signup.jsx';
+export { Home } from '@pages/Home/Home.jsx';
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Login, Signup } from '@pages';
+import { Login, Signup, Home } from '@pages';
 import '@/App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />}></Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="signup" element={<Signup />}></Route>
       </Routes>
