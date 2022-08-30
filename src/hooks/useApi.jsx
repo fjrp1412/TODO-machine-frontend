73da4cba64da9b916ca6b7770ec975aa45ebc7c6
Francisco Ruiz <fruz.rc@gmail.com>
2022-08-30 10:03:22 -0400

    trying again

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-const { API_URL } = process.env;
+
+const API_URL = import.meta.env.API_URL
 
 const TYPES = {
   POST: 'POST',
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const { API_URL } = process.env;
+const API_URL = import.meta.env.API_URL
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
